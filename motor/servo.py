2398ac11949b6c9afbde93ce6d79c1ca56122e9d
Adam Driggers <awdriggs@gmail.com>
2025-03-04 11:52:05 +0100

    update

---
+++ motor/servo.py
@@ -0,0 +1,54 @@
+# wiring 
+# - red to 5v or 3.3v depending on the servo
+# - black to ground of course
+# - yello to pin 28
+
+import time
+import board
+from digitalio import DigitalInOut, Direction, Pull
+import pwmio
+from adafruit_motor import servo #make sure to include the library on the pico
+
+print("servo test program")
+
+# Servo setup
+pwm_servo = pwmio.PWMOut(board.GP28, duty_cycle=2 ** 15, frequency=50)
+servo1 = servo.Servo(
+    pwm_servo, min_pulse=500, max_pulse=2200
+)  # tune pulse for specific servo
+
+# Two function definitions for servo tests
+
+# Servo test
+def servo_direct_test():
+    print("servo test: 90")
+    servo1.angle = 90
+    time.sleep(2)
+    print("servo test: 0")
+    servo1.angle = 0
+    time.sleep(2)
+    print("servo test: 90")
+    servo1.angle = 90
+    time.sleep(2)
+    print("servo test: 180")
+    servo1.angle = 180
+    time.sleep(2)
+    
+# Servo smooth test
+def servo_smooth_test():
+    print("servo smooth test: 180 - 0, -1º steps")
+    for angle in range(180, 0, -1):  # 180 - 0 degrees, -1º at a time.
+        servo1.angle = angle
+        time.sleep(0.01)
+    time.sleep(1)
+    print("servo smooth test: 0 - 180, 1º steps")
+    for angle in range(0, 180, 1):  # 0 - 180 degrees, 1º at a time.
+        servo1.angle = angle
+        time.sleep(0.01)
+    time.sleep(1)
+    
+# main loop
+while True:
+    # uncomment to run a test
+    # servo_direct_test()
+    servo_smooth_test() 
